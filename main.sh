e7e747a3520a6a7c4a52a776730944d94d9554ce
jdoblasuned <jdoblas4@alumno.uned.es>
2025-11-16 09:30:56 +0100

    El script funciona, a falta de hacer comprobaciones completas y pendiente de revisión final

--- main.sh
+++ main.sh
@@ -1,22 +1,6 @@
 #! /bin/bash
 
-###########################################
-#                                         #
-#         Archivos de registros           #
-#                                         #
-###########################################
 
-if [ ! -e ~/aeropuerto/data ]; then
-  if [ ! -e ~/aeropuerto ]; then 
-    mkdir ~/aeropuerto
-    touch ~/aeropuerto/data
-  else
-    touch ~/aeropuerto/data
-  fi 
-fi
-
-log=~/aeropuerto/data
-temp_log=~/aeropuerto/temp
 
 
 ###########################################
@@ -201,44 +185,8 @@ ask_name () {
 
 
 #############################################
-#       Solicitar datos del pasajero        #
+#       Gestionar datos del pasajero        #
 #############################################
-
-ask_data () {
-  read -r -p "Nombre y apellidos del pasajero: "   name
-  read -r -p "Identificador del vuelo: "           flight
-  read -r -p "Día del vuelo: "                     day
-  read -r -p "Mes del vuelo (numérico): "          month
-  read -r -p "Año del vuelo: "                     year
-  read -r -p "Destino: "                           destination
-}
-
-
-#############################################
-#        Mostrar número de registro         # 
-#############################################
-
-reg_number() {
-  local name=$*
-
-  awk -F '|' -v name="$name" 'tolower($1) ~ tolower(name) {print NR "|" $0} ' $log > $temp_log 
-  awk -F '|' -v name="$name" 'BEGIN{ printf "\nCoincidencias:\n" } 
-  tolower($2) ~ tolower(name) { printf "\nNúmero de registro: %s    |Pasajero: %s    |Vuelo: %s    |Fecha: %s de %s de %s   |Destino: %s\n\n",$1,$2,$3,$4,$5,$6,$7 } ' $temp_log | column -t -s "|"
-}
-
-
-###########################################
-#                                         #
-#          Funciones del programa         #
-#                                         #
-###########################################
-
-
-
-#############################################
-#          Registrar pasajeros              #
-#############################################
- 
 
 save_data () {
   local name=$1
@@ -249,10 +197,10 @@ save_data () {
   local destination=$6
 
   if  ! is_right_name "$name" ; then
-    echo "El nombre solo puede contener letras y espacios"
+    echo 'El nombre solo puede contener letras y espacios'
     return 1
   elif  ! is_right_date $day $month $year ; then
-    echo "Elige una fecha correcta" 
+    echo 'La fecha no es correcta'
     return 1
   fi
 
@@ -272,111 +220,29 @@ save_passenger() {
     END{printf "%s   |%s   |%s de %s de %s   |%s\n",$1,$2,$3,$4,$5,$6}' $log | column -t -s '|'
     return 0
   else
-    echo "Se ha producido un error al guardar"  
+    echo 'Se ha producido un error al guardar'
     return 1
   fi
 
   rm $temp_log
 }
 
-ask_passenger () {
-  ask_data
-  until save_data "$name" "$flight" $day $month $year "$destination"; do 
-    ask_data 
-  done
-  save_passenger 
-}
 
+#############################################
+#        Mostrar número de registro         # 
+#############################################
 
-##############################################
-#       Listar pasajeros de un vuelo         #
-##############################################
-  
-#	Buscar id vuelo e imprimir registros que coincidan.
-#	Se puede añadir argumentos variables para 
-
-list_flight () {
-  local flight=$1
-
-#  awk -F '|' -v flight="$flight" 'BEGIN{ printf "\nPasajeros del vuelo %s:\n", flight } 
-#  $2 == flight { printf "- %-40s %-2s de %-10s de %-5s %-25s\n",$1,$3,$4,$5,$6 } ' $log
-
-awk -F '|' -v flight="$flight" 'BEGIN{ printf "\nPasajeros del vuelo %s:\n", flight } 
-$2 == flight { printf "- %s    |%s de %s de %s    |%s\n", $1,$3,$4,$5,$6 } ' $log | column -t -s '|'
-
-}
-
-
-##############################################
-#            Buscar pasarjero                #
-##############################################
-#		Pag. 169 bash.pdf
-#	Se podría poner argumentos variables para buscar directamente con main.sh buscar nombre pasajero
-
-search_psg () {
+reg_number() {
   local name=$*
 
-#  awk -F '|' -v name="$name" 'BEGIN{ printf "\nResultados encontrados:\n" } 
-#  tolower($1) ~ tolower(name) { printf "Pasajero: %-40s Vuelo: %-8s Fecha: %-2s de %-10s de %-8s Destino: %-25s\n",$1,$2,$3,$4,$5,$6 } ' $log
-  awk -F '|' -v name="$name" 'BEGIN{ printf "\nResultados encontrados:\n" } 
-  tolower($1) ~ tolower(name) { printf "Pasajero: %s    |Vuelo: %s    |Fecha: %s de %s de %s   |Destino: %s\n",$1,$2,$3,$4,$5,$6 } ' $log | column -t -s '|'
+  awk -F '|' -v name="$name" 'tolower($1) ~ tolower(name) {print NR "|" $0} ' $log > $temp_log 
+  awk -F '|' -v name="$name" 'BEGIN{ printf "\nCoincidencias:\n" } 
+  tolower($2) ~ tolower(name) { printf "\nNúmero de registro: %s    |Pasajero: %s    |Vuelo: %s    |Fecha: %s de %s de %s   |Destino: %s\n\n",$1,$2,$3,$4,$5,$6,$7 } ' $temp_log | column -t -s "|"
 }
 
-
-##############################################
-#            Eliminar pasajero               #
-##############################################
-
-### Función de búsqueda adaptada a la eliminación de pasajero ###
-# Se muestra el número de registro para poder elegir la coincidencia a eliminar
-
-del_pass () {
-  local name=$*
-  local n_pass
-
-  reg_number $name 
-
-  echo 'Confirma el pasajero a eliminar introduciendo el número de registro'
-  read n_pass 
-   
-  printf  "¿Quieres eliminar el registro: $(awk -F '|' -v num="$n_pass" 'NR==num {printf "%s  |%s  |%s de %s de %s  |%s\n", $1,$2,$3,$4,$5,$6}' $log | column -t -s '|') ?\n"
-  
-  select option in "Sí" "No"
-  do
-    case $REPLY in
-      1) gsed -i  ""$n_pass"d" $log; break;;
-      2) break;;
-    esac
-  done    
-  rm $temp_log 
-}
-
-
-##############################################
-#             Modificar registro             #
-##############################################
-
-modif_psgr () {
-  local name=$*
-  local n_pass
-
-  reg_number $name
-  read -r -p "Confirma el registro a modificar con el número de registro: " n_pass
-  
-  
-  printf  "Registro: $(awk -F '|' -v num="$n_pass" 'NR==num {printf "%s  |%s  |%s de %s de %s  |%s\n", $1,$2,$3,$4,$5,$6}' $log | column -t -s '|')\n"
-  echo '#################'
-  echo '¿Quieres modificarlo?'
-  echo '#################'
-  select option in "Sí" "No"
-  do
-    case $REPLY in 
-      1) modif_data $n_pass
-         break;;
-      2) break;;
-    esac
-  done
-}
+#############################################
+#       Modificar datos de un registro      #
+#############################################
 
 modif_data (){
   local name flight day month year destination
@@ -394,7 +260,6 @@ modif_data (){
   while true; do 
   echo '#################'
   echo 'Elige una opción:'
-  echo '#################'
     COLUMNS=1
     select option in "Modificar nombre" "Modificar vuelo" "Modificar fecha" "Modificar destino" "Guardar" "Salir sin guardar"
     do 
@@ -429,11 +294,166 @@ modif_data (){
    done
 }
 
+
+###########################################
+#                                         #
+#          Funciones del programa         #
+#                                         #
+###########################################
+
+
+
+#############################################
+#          Registrar pasajeros              #
+#############################################
+ 
+menu_register() {
+  local name flight day month year destination 
+
+  read -r -p "Nombre y apellidos del pasajero: "   name
+  read -r -p "Identificador del vuelo: "           flight
+  read -r -p "Día del vuelo: "                     day
+  read -r -p "Mes del vuelo (numérico): "          month
+  read -r -p "Año del vuelo: "                     year
+  read -r -p "Destino: "                           destination
+
+  register_passenger "$name" "$flight" "$day" "$month" "$year" "$destination"
+}
+
+register_passenger () {
+  if save_data "$1" "$2" "$3" "$4" "$5" "$6"; then
+    save_passenger
+  else
+    echo "Error en los datos"
+    return 1
+  fi
+}
+
+
+##############################################
+#       Listar pasajeros de un vuelo         #
+##############################################
+  
+#	Buscar id vuelo e imprimir registros que coincidan.
+#	Se puede añadir argumentos variables para 
+
+menu_list () {
+  local flight
+  read -r -p "Ingrese el identificador del vuelo: " flight
+  list_flight "$flight"
+}
+
+list_flight () {
+  local flight=$1
+
+#  awk -F '|' -v flight="$flight" 'BEGIN{ printf "\nPasajeros del vuelo %s:\n", flight } 
+#  $2 == flight { printf "- %-40s %-2s de %-10s de %-5s %-25s\n",$1,$3,$4,$5,$6 } ' $log
+
+awk -F '|' -v flight="$flight" 'BEGIN{ printf "\nPasajeros del vuelo %s:\n", flight } 
+$2 == flight { printf "- %s    |%s de %s de %s    |%s\n", $1,$3,$4,$5,$6 } ' $log | column -t -s '|'
+
+}
+
+
+##############################################
+#            Buscar pasarjero                #
+##############################################
+#		Pag. 169 bash.pdf
+#	Se podría poner argumentos variables para buscar directamente con main.sh buscar nombre pasajero
+
+menu_search () {
+  local name
+  read -r -p "Ingrese nombre o parte del nombre: " name
+
+  search_passenger "$name"
+}
+
+search_passenger () {
+  local name=$*
+
+#  awk -F '|' -v name="$name" 'BEGIN{ printf "\nResultados encontrados:\n" } 
+#  tolower($1) ~ tolower(name) { printf "Pasajero: %-40s Vuelo: %-8s Fecha: %-2s de %-10s de %-8s Destino: %-25s\n",$1,$2,$3,$4,$5,$6 } ' $log
+  awk -F '|' -v name="$name" 'BEGIN{ printf "\nResultados encontrados:\n" } 
+  tolower($1) ~ tolower(name) { printf "Pasajero: %s    |Vuelo: %s    |Fecha: %s de %s de %s   |Destino: %s\n",$1,$2,$3,$4,$5,$6 } ' $log | column -t -s '|'
+}
+
+
+##############################################
+#            Eliminar pasajero               #
+##############################################
+
+### Función de búsqueda adaptada a la eliminación de pasajero ###
+# Se muestra el número de registro para poder elegir la coincidencia a eliminar
+
+menu_del () {
+  local name
+
+  read -r -p "Ingrese nombre o parte del nombre: " name
+
+  del_passenger "$name"
+}
+
+del_passenger () {
+  local name=$*
+  local n_pass
+
+  reg_number $name 
+
+  echo 'Confirma el pasajero a eliminar introduciendo el número de registro'
+  read n_pass 
+   
+  printf  "¿Quieres eliminar el registro: $(awk -F '|' -v num="$n_pass" 'NR==num {printf "%s  |%s  |%s de %s de %s  |%s\n", $1,$2,$3,$4,$5,$6}' $log | column -t -s '|') ?\n"
+  
+  select option in "Sí" "No"
+  do
+    case $REPLY in
+      1) gsed -i  ""$n_pass"d" $log; break;;
+      2) break;;
+    esac
+  done    
+  rm $temp_log 
+}
+
+
+##############################################
+#             Modificar registro             #
+##############################################
+
+menu_modif () {
+  local name
+
+  read -r -p "Ingrese nombre o parte del nombre: " name
+
+  modif_passenger "$name"
+}
+
+
+modif_passenger () {
+  local name=$*
+  local n_pass
+
+  reg_number $name
+  read -r -p "Confirma el registro a modificar con el número de registro: " n_pass
+  
+  printf  "Registro: $(awk -F '|' -v num="$n_pass" 'NR==num {printf "%s  |%s  |%s de %s de %s  |%s\n", $1,$2,$3,$4,$5,$6}' $log | column -t -s '|')\n"
+  echo '#################'
+  echo '¿Quieres modificarlo?'
+  select option in "Sí" "No"
+  do
+    case $REPLY in 
+      1) modif_data $n_pass
+         break;;
+      2) break;;
+    esac
+  done
+}
+
 ##############################################
 #        Consultar próximos vuelos           #
 ##############################################
 # ToDo: 
 #	Explicar -A -c -m -o 
+
 next_flights () {
 echo "Próximos vuelos: " 
 ps -Acmo pid,command,pmem,pcpu | head -n 6
@@ -442,9 +462,88 @@ ps -Acmo pid,command,pmem,pcpu | head -n 6
 
 ###########################################
 #                                         #
-#          Programa principal             #
+# ######### PROGRAMA PRINCIPAL ########## #
 #                                         #
 ###########################################
 
-ask_passenger
 
+###########################################
+#         Archivos de registros           #
+###########################################
+
+init_storage () {
+  if [ ! -e ~/aeropuerto/data ]; then
+    if [ ! -e ~/aeropuerto ]; then 
+      mkdir ~/aeropuerto
+      touch ~/aeropuerto/data
+    else
+      touch ~/aeropuerto/data
+    fi 
+  fi
+
+  log=~/aeropuerto/data
+  temp_log=~/aeropuerto/temp
+}
+
+
+###########################################
+#                 Menú                    #
+###########################################
+menu () { 
+  while true; do
+      clear
+      echo "=============================================="
+      echo "     Sistema de Gestión - Aeropuerto Regional"
+      echo "=============================================="
+      echo "          1. Registrar pasajero y vuelo"
+      echo "          2. Listar pasajeros de un vuelo"
+      echo "          3. Buscar pasajero"
+      echo "          4. Eliminar pasajero"
+      echo "          5. Modificar registro"
+      echo "          6. Consultar próximos vuelos"
+      echo "          7. Salir"
+      echo "=============================================="
+      read -p "Seleccione una opción [1-7]: " opcion
+
+    case $opcion in
+      1) menu_register;;
+      2) menu_list;;
+      3) menu_search;;
+      4) menu_del;;
+      5) menu_modif;;
+      6) next_flights;;
+      7) echo "Saliendo del programa"
+         sleep 1
+         break;;
+      *) echo "Opción inválida. Intente de nuevo."
+         sleep 1;;
+
+    esac
+
+      echo
+      read -p "Presione Enter para continuar..."
+  done
+}
+
+
+###########################################
+#                 MAIN                    #
+###########################################
+
+main () {
+  init_storage
+  local option="${1:-}"
+  case $option in
+    registrar) shift; if [ -z "${1:-}" ]; then menu_register; else register_passenger "$@"; fi ;;
+    listar) shift; if [ -z "${1:-}" ]; then menu_list ; else list_flight "$@" ; fi  ;;
+    buscar) shift ; if [ -z "${1:-}" ]; then menu_search ; else search_passenger "$@" ; fi ;;
+    eliminar) shift ; if [ -z "${1:-}" ]; then menu_del ; else del_passenger "$@" ; fi ;;
+    modificar) shift ; if [ -z "${1:-}" ]; then menu_modif ; else modif_passenger "$@" ; fi ;;
+    consultar) next_flights ;;
+    "") menu ;;
+    *) echo "Opción no válida";;
+  esac
+}
+
+
+main "$@"
